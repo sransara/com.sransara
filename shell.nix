2372d735e73ac34b6d11e5f4849842da6730f89c
sransara <git@sransara.com>
2019-10-12 14:29:48 -0700

    make latex a global package

--- shell.nix
+++ shell.nix
@@ -1,15 +1,9 @@
 with import <nixpkgs> {};
 
 pkgs.mkShell {
-  name = "node";
   buildInputs = [
-      nodejs
       hugo
-      (texlive.combine { 
-	inherit (texlive) scheme-medium enumitem;
-      })
   ];
   shellHook = ''
-    export PATH="$PWD/node_modules/.bin/:$PATH"
   '';
 }
