04cd7c47713c1f1fec7abb06acabe17e50b56383
sransara <git@sransara.com>
2019-12-18 07:29:59 -0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,66 @@ const cp = require("child_process");
 
 const del = require("del");
 const gulp = require("gulp");
-const gulpChanged = require('gulp-changed');
+const gulpChanged = require("gulp-changed");
 const gulpPlumber = require("gulp-plumber");
-const through = require('through2');
-
+const pluginError = require("plugin-error");
+const through = require("through2");
+const vinyl = require("vinyl");
 
 function mdxBuild() {
-  return gulp.src("./content/**/index.md", { base: '.' })
+  return gulp
+    .src("./content/**/index.md", { base: "." })
     .pipe(gulpPlumber())
     .pipe(gulpChanged("./transient/"))
-    .pipe(through.obj(function (file, enc, cb) {
-      file.path = file.path.replace(/index.md$/, 'index.json')
-      return cb(null, file)
-    }))
+    .pipe(
+      through.obj(function(file, enc, cb) {
+        PLUGIN_NAME = "index-md-json";
+        if (file.isNull()) {
+          return cb(null, file);
+        }
+        if (file.isStream()) {
+          this.emit(
+            "error",
+            new pluginError(PLUGIN_NAME, "Streams are unsupported")
+          );
+          return cb();
+        }
+        nfile = file.clone();
+        nfile.path = nfile.path.replace(/index.md$/, "index.json");
+        console.log(
+          file.contents.toString(enc).matchAll(/{{<named-(\w+).*?>}}/m)
+        );
+        return cb(null, nfile);
+      })
+    )
     .pipe(gulp.dest("./transient/"));
 }
 
 function styleBuild() {
-  return gulp.src("./assets/styles/main.css", { base: '.' })
+  return gulp
+    .src("./assets/styles/main.css", { base: "." })
     .pipe(gulpPlumber())
-    .pipe(require("gulp-postcss")([
-      require("tailwindcss")("./assets/styles/tailwind.config.js"),
-      require('postcss-nested')(),
-      require("@fullhuman/postcss-purgecss")({
-        content: ["layouts/**/*.html"],
-        extractors: [{
-          extractor: class TailwindExtractor {
-            static extract(content) {
-                return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    .pipe(
+      require("gulp-postcss")([
+        require("tailwindcss")("./assets/styles/tailwind.config.js"),
+        require("postcss-nested")(),
+        require("@fullhuman/postcss-purgecss")({
+          content: ["layouts/**/*.html"],
+          extractors: [
+            {
+              extractor: class TailwindExtractor {
+                static extract(content) {
+                  return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+                }
+              },
+              extensions: ["html"]
             }
-          },
-          extensions: ["html"]
-        }],
-        fontFace: true,
-      }),
-      require("autoprefixer")(),
-    ]))
+          ],
+          fontFace: true
+        }),
+        require("autoprefixer")()
+      ])
+    )
     .pipe(gulp.dest("./transient/"));
 }
 
@@ -51,11 +75,15 @@ function mdxClean() {
 
 const transientBuild = gulp.parallel(
   gulp.series(styleClean, styleBuild),
-  gulp.series(mdxClean, mdxBuild),
+  gulp.series(mdxClean, mdxBuild)
 );
 
 function transientWatch() {
-  gulp.watch(["./assets/styles/**", "layouts/**/*.html"],  { delay: 500 }, styleBuild);
+  gulp.watch(
+    ["./assets/styles/**", "layouts/**/*.html"],
+    { delay: 500 },
+    styleBuild
+  );
 }
 
 function siteServe() {
@@ -63,20 +91,16 @@ function siteServe() {
 }
 
 function siteBuild() {
-  return cp.spawn("hugo", ["--minify", "--templateMetrics", "--templateMetricsHints"], { stdio: "inherit" });
+  return cp.spawn(
+    "hugo",
+    ["--minify", "--templateMetrics", "--templateMetricsHints"],
+    { stdio: "inherit" }
+  );
 }
 
-
-gulp.task("serve",
-  gulp.series(
-    transientBuild,
-    gulp.parallel(siteServe, transientWatch),
-  )
+gulp.task(
+  "serve",
+  gulp.series(transientBuild, gulp.parallel(siteServe, transientWatch))
 );
 
-gulp.task("publish",
-  gulp.series(
-    transientBuild,
-    siteBuild,
-  )
-);
+gulp.task("publish", gulp.series(transientBuild, siteBuild));
