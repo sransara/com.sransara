ab58f8e840dcda60d01488f525bed511eda87f85
Samodya Abeysiriwardane <git@sransara.com>
2023-02-25 08:02:07 -0600

    Add header section

--- src/text/notes/2019/build-yourself-a-git/metadata.ts
+++ src/text/notes/2019/build-yourself-a-git/metadata.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from '@src/lib/types/notes';
 
 export const metadata: Metadata = {
-  title: 'Build yourself a git conceptually',
+  title: 'Build yourself a git',
   publishedDate: '2019-02-28'
 };
