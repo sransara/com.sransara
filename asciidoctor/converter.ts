054239ba22bf2b31cb73c635177382745c7f3fa8
Samodya Abeysiriwardane <git@sransara.com>
2023-02-25 19:35:26 -0600

    Refactor converter

--- asciidoctor/converter.ts
+++ asciidoctor/converter.ts
@@ -1,17 +1,25 @@
 // @ts-nocheck
+class InlineMath {
+  static match(node) {
+    return (
+      node.getNodeName() === 'inline_quoted' && ['asciimath', 'latexmath'].includes(node.getType())
+    );
+  }
+  static convert(node, transform, opts) {
+    return `<span class="math" data-lang="${node.getType()}">${node.getText()}</span>`;
+  }
+}
+
 class CustomConverter {
   constructor(asciidoctorEngine) {
     this.baseConverter = asciidoctorEngine.Html5Converter.create();
   }
-  convert(node, transform) {
-    const base = this.baseConverter.convert(node, transform);
-    if (node.getNodeName() !== 'inline_quoted') {
-      return base;
+
+  convert(node, transform, opts) {
+    if (InlineMath.match(node)) {
+      return InlineMath.convert(node, transform, opts);
     }
-    if (!['asciimath', 'latexmath'].includes(node.getType())) {
-      return base;
-    }
-    return `<span class="math" data-lang="${node.getType()}">${node.getText()}</span>`;
+    return this.baseConverter.convert(node, transform, opts);
   }
 }
 
