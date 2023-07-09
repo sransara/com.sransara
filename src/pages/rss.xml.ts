213d3656555eeee59755989df8baab12c1aa12bd
Samodya Abeysiriwardane <git@sransara.com>
2023-07-08 19:22:39 -0500

    WIP rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,13 @@
+import rss from '@astrojs/rss';
+import type { APIRoute } from 'astro';
+import siteValues from '~/site.values.mjs';
+
+export const get: APIRoute = async function get() {
+  return rss({
+    title: siteValues.name,
+    description: siteValues.tagLine,
+    site: siteValues.site,
+    customData: '<language>en</language>',
+    items: []
+  });
+};
