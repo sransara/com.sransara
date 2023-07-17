5f2953489891b314344413983ca3bd0473620826
Samodya Abeysiriwardane <git@sransara.com>
2023-07-17 02:23:08 -0500

    Add new note

--- src/text/notes/2023/bazel-python-venv/metadata.ts
+++ src/text/notes/2023/bazel-python-venv/metadata.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from '@src/lib/types/notes';
 
 export const metadata: Metadata = {
-  title: 'Build yourself a Git',
-  publishedDate: '2019-02-28'
+  title: 'Quick guide: Bazel Python virtual environment',
+  publishedDate: '2023-05-05'
 };
