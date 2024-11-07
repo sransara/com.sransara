f8344275fbc62f00d868c3cc9ca208c80bb24eb0
Samodya Abeysiriwardane <git@sransara.com>
2024-11-07 02:15:45 -0600

    fix inline_callout
    inline_callout is used in both paragraphs and code blocks.

--- adocx/nodeConverters/inline_callout.ts
+++
@@ -1,8 +0,0 @@
-import type { Inline } from 'asciidoctor';
-
-import { type AdocNodeConverter } from '#adocx/nodeConvertingConverter';
-
-export const convert: AdocNodeConverter<Inline> = (node: Inline, _opts?: any) => {
-  const text = node.getText();
-  return `\u0D82${text}\u0D83`;
-};
